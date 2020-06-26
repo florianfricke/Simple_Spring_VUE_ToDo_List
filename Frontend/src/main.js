6fad1fb3b1ff40d85554808bd35465a7e901df62
Florian Fricke <florian_fricke@gmx.de>
2020-06-26 13:37:45 +0200

    frontend: change order of script imports

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './router'
+import Vue from 'vue'
+import App from './App'
 
 Vue.config.productionTip = false
 
